ad623b29cc7e9e8b2184705c0b6f7144c979b0cd
anaclarapro99 <anaclaratavares145@gmail.com>
2025-05-06 20:24:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ if st.button("🎲 Surpreenda-me!"):
         st.write(quote_en)
 
         with st.expander("🔍 Ver texto original (português)"):
-            st.write(f"Frase original: {quote_pt}")
+            st.write(f"Frase original: {quote_en}")
             
