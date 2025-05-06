c546abc1cea44e3ba89a20b8cb2e6ece516b511d
anaclarapro99 <anaclaratavares145@gmail.com>
2025-05-05 21:17:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ st.markdown("### 📌 Versão em português")
 frase_pt = traduzir_para_portugues('quote')
 
  st.subheader("🎬 Frase traduzida")
-        st.write(f"📝 {frase_pt}")
+ st.write(f"📝 {frase_pt}")
 
 with st.expander("🔍 Ver texto original (português)"):
     st.write(f"**Frase original:** (quote)")
